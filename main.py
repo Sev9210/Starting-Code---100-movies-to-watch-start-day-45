02c6096e9937c805839ebd9f67988f772b8a26ba
SEV <alienlablabko@gmail.com>
2026-05-23 21:15:41 +0800

    Initial commit - 100 Movies Scraper

---
+++ main.py
@@ -0,0 +1,20 @@
+# Write your code below this line 👇
+import requests
+from bs4 import BeautifulSoup
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(url=URL)
+movies = response.text
+
+soup = BeautifulSoup(movies, 'html.parser')
+
+
+movies = soup.select('div.article-title-description__text h3')
+top_one_hundred_movies = sorted(title.getText()for title in movies)
+
+with open('Movies', 'w', encoding='utf-8') as fav_movies:
+    for title in top_one_hundred_movies:
+        fav_movies.write(f'{title}\n')
+
+
+print(top_one_hundred_movies)
